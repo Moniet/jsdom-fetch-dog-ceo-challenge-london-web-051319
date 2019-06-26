34f3a01a37a5fb5ba557089daa13d99700cec985
moniet <moniet.sawhney@gmail.com>
2019-06-26 21:13:55 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = 'https://dog.ceo/api/breeds/image/random/4';
+const breeds = [];
+
+function getImgs() {
+  fetch(imgUrl)
+    .then(dogs => dogs.json())
+    .then(dogs => loadDogImgs(dogs));
+}
+
+function loadDogImgs(obj) {
+  const arr = obj['message'];
+
+  arr.forEach(url => {
+    const img = document.createElement('img');
+    const container = document.querySelector('#dog-image-container');
+
+    img.setAttribute('src', url);
+    container.appendChild(img);
+
+    addBreed(url);
+  });
+}
+
+function addBreed(url) {
+  const breed = url.split('/')[4];
+  const container = document.querySelector('#dog-breeds');
+  const li = document.createElement('li');
+
+  li.textContent = breed;
+  container.appendChild(li);
+
+  breeds.push(breed);
+}
+
+function selectDog() {
+  const container = document.querySelector('#dog-breeds');
+  let letter = this.value;
+  let selected = breeds.filter(breed => breed[0] === letter);
+
+  container.textContent = '';
+  selected.forEach(breed => {
+    const li = document.createElement('li');
+    li.textContent = breed;
+
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', e => {
+
+  const select = document.querySelector('#breed-dropdown');
+  const ul = document.querySelector('#dog-breeds');
+
+  select.addEventListener('change', selectDog);
+
+  ul.addEventListener('click', e => {
+    if (e.target && e.target.nodeName == 'LI') {
+      e.target.style.color = 'pink';
+    }
+  });
+
+  getImgs();
+
+});
